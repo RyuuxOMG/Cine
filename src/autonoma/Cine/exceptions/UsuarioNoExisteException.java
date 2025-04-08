3bd4376bbd8c60c3512d3ca910073631de27e1f9
Juan Manuel Ochoa <juanm.ochoamu@autonoma.edu.co>
2025-04-07 21:50:44 -0500

    Se comenta usuario

--- src/autonoma/Cine/exceptions/UsuarioNoExisteException.java
+++ src/autonoma/Cine/exceptions/UsuarioNoExisteException.java
@@ -3,7 +3,7 @@ package autonoma.Cine.exceptions;
 // Excepcion personalizada que se lanza cuando un usuario no se encuentra
 public class UsuarioNoExisteException extends Exception {
 
-    // Constructor que recibe un mensaje personalizado
+    
     public UsuarioNoExisteException(String mensaje) {
         super("El usuario ingresado no se encuentra registrado.");
     }
