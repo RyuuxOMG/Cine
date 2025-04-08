e04bca3b2bceadc448f6b2c6eedc4969c5b408b4
Juan Manuel Ochoa <juanm.ochoamu@autonoma.edu.co>
2025-04-07 21:51:09 -0500

    Se comenta pelicula

--- src/autonoma/Cine/exceptions/PeliculaNoExisteException.java
+++ src/autonoma/Cine/exceptions/PeliculaNoExisteException.java
@@ -3,7 +3,7 @@ package autonoma.Cine.exceptions;
 // Excepcion personalizada que se lanza cuando una pelicula no se encuentra
 public class PeliculaNoExisteException extends Exception {
 
-    // Constructor que recibe un mensaje personalizado
+    
     public PeliculaNoExisteException(String mensaje) {
         super("La pelicula ingresada no existe en cartelera.");
     }
